70306c7f1164acfef850c480bd11528fe2d489fd
benamiller <mrbenamiller@gmail.com>
2025-03-24 20:41:28 +0000

    Formatting and neovim updates

--- src/Main.elm
+++ src/Main.elm
@@ -1,27 +1,69 @@
 module Main exposing (main)
 
 import Browser
+import Browser.Navigation as Nav
 import Html exposing (Html, div, h1, text)
+import Url exposing (Url)
+
+
 
 -- MODEL
-type alias Model = { title : String }
 
-init : Model
-init = { title = "Ben" }
+
+type alias Model =
+    { key : Nav.Key, currentPage : Page }
+
+
+type Page
+    = Home
+    | About
+    | Portfolio
+
+
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+    ( { key = key, currentPage = urlToPage url }, Cmd.none )
+
+
 
 -- UPDATE
-type Msg = NoOp
 
-update : Msg -> Model -> Model
-update msg model = model
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | currentPage = urlToPage url }, Cmd.none )
+
+
 
 -- VIEW
-view : Model -> Html Msg
-view model = 
-        div []
-                [ h1 [] [ text model.title ]
-                , text "Hello"
-                ]
+
+
+view : Model -> Browser.Document Msg
+view model =
+    div []
+        [ h1 [] [ text model.title ]
+        , text "Hello"
+        ]
+
+
 
 -- PROGRAM
-main = Browser.sandbox { init = init, update = update, view = view }
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
